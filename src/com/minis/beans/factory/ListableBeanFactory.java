052559dfac4984d983f2fb986862d3e59ab32b15
郑超伟 <15204782+zhengchaowei8812@user.noreply.gitee.com>
2025-01-16 23:56:38 +0800

    手写spring

--- src/com/minis/beans/factory/ListableBeanFactory.java
+++ src/com/minis/beans/factory/ListableBeanFactory.java
@@ -4,6 +4,12 @@ import java.util.Map;
 
 import com.minis.beans.BeansException;
 
+/**
+ * ListableBeanFactory 是 Spring 框架中 BeanFactory 接口的一个扩展。它提供了额外的功能
+ * 允许你查询和操作在容器中定义的 bean 的列表。这个接口继承自 BeanFactory，
+ * 并添加了一些新的方法，如 getBeanNamesForType(Class<?> type)
+ * 和 getBeansOfType(Class<?> type)，这些方法允许你基于类型来获取 bean 的名称和实例。
+ */
 public interface ListableBeanFactory extends BeanFactory {
 
 	boolean containsBeanDefinition(String beanName);
